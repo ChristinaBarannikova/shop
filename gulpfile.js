06eb7e84cc8c0e32b142a189f69dbefbe09cbe66
ChristinaBarannikova <ChristinaBarannikova@users.noreply.github.com>
2017-10-27 08:35:19 +0300

    ejs

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,9 @@ const gulp = require('gulp');
 const less = require('gulp-less');
 const browserSync = require('browser-sync');
 const autoprefixer = require('gulp-autoprefixer');
+const rename       = require('gulp-rename');
+const ejs          = require('gulp-ejs');
+const gutil        = require('gulp-util');
 
 // Автоперезагрузка при изменении файлов в папке `dist`:
 // Принцип: меняем файлы в `/src`, они обрабатываются и переносятся в `dist` и срабатывает автоперезагрузка.
@@ -37,8 +40,15 @@ gulp.task('js', () => {
         .pipe(gulp.dest('./dist/js'));
 });
 
+// gulp.task('html', () => {
+//     gulp.src('src/index.html')
+//         .pipe(gulp.dest('./dist'));
+// });
+
 gulp.task('html', () => {
-    gulp.src('src/index.html')
+    gulp.src('src/index.ejs')
+    .pipe(ejs().on('error', gutil.log))
+    .pipe(rename('index.html'))
         .pipe(gulp.dest('./dist'));
 });
 
